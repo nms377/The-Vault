21a585e5d27b2acda379f9247789d8829f87ed8e
nms377 <nms377@nyu.edu>
2016-11-22 00:32:47 -1000

    Got my code to pass for all 4 tests. set a return property for setValue and getValue functions.

--- vault.js
+++ vault.js
@@ -1,4 +1,29 @@
 'use strict';
 module.exports = function() {
-  
+
+//Create variables store value and key
+	var _key = 0;
+	var _value = 0;
+
+//sets value in vault identified by its key
+function setValue(key, value){
+	_key = key;
+	_value = value;
+	return getValue();
+}
+
+//if key exists return the value, if key doesn't exist, return null
+function getValue(key){
+	if(_key){
+		return _value;
+	}else{
+		return null;
+	}
+}
+
+
+return{
+	setValue: setValue,
+	getValue: getValue
+};
 };
